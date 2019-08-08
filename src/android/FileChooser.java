dd4406192217adf88a63e605b4a7c9640b634e66
Mr. Liu <liuyx.android@gmail.com>
2019-08-08 11:30:17 +0800

    Update FileChooser.java
    兼容Android6.0以下

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -35,8 +35,10 @@ public class FileChooser extends CordovaPlugin {
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
     String[] permissionArray ={"android.permission.READ_EXTERNAL_STORAGE","android.permission.WRITE_EXTERNAL_STORAGE"};
     cordova.requestPermissions(this, 55433,permissionArray);
+	}
   }
 
   @Override
